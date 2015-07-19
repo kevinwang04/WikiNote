8b892b79700f229b85e7da8c3523d3fd78ec2bf5
Li Xinyang <lixinyang1026@gmail.com>
2015-07-19 23:24:19 +0800

    fix app.js typos

--- app/script/app.js
+++ app/script/app.js
@@ -1,4 +1,4 @@
-requirejs.config({
+require.config({
   paths: {
     jquery: '../bower_components/jquery/dist/jquery.min',
     marked: '../bower_components/marked/marked.min',
@@ -6,6 +6,8 @@ requirejs.config({
   }
 });
 
-requirejs(['jquery', 'data'], function ($, data) {
-
+// Start the main app logic
+require(['jquery', 'data'], function ($, data) {
+  $('body').css('background', 'darkblue');
+  console.log(data.getAllNotebook());
 });
