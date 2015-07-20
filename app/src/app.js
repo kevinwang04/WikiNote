79a70c3e0a0cc252b05b9152150e1c5fcb3df854
Heon_Gu <514720858@qq.com>
2015-07-20 16:24:12 +0800

    模块名称及dom操作修改

--- app/src/app.js
+++ app/src/app.js
@@ -3,12 +3,14 @@ require.config({
   paths: {
     jquery: '../bower_components/jquery/dist/jquery.min',
     marked: '../bower_components/marked/marked.min',
-    data: './data'
+    data: './data',
+    showNotebookList: './showNotebookList'
   }
 });
 
 // Start the main app logic
-require(['jquery', 'data'], function ($, data) {
+require(['jquery', 'data','show'], function ($, data,showNotebookList) {
   $('body').css('background', 'darkblue');
   console.log(data.getAllNotebook());
+  console.log(showNotebookList.notebooks);
 });
